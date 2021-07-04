0caeb2bed3c3e572c8dc0f8ff57f97ccc82f404c
Juan Moreno <juan.moreno@geniussports.com>
2021-07-03 20:54:51 -0500

    first commit

---
+++ src/main/kotlin/com/example/toshlautomator/ToshlAutomatorApplication.kt
@@ -0,0 +1,11 @@
+package com.example.toshlautomator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToshlAutomatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<ToshlAutomatorApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/toshlautomator/ToshlAutomatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.toshlautomator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToshlAutomatorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
